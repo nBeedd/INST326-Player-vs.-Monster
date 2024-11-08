35cf5bbad98654cefd7a51a806a3c6e504a1e685
Ahmed Babikir <ahmed@Ahmeds-MacBook-Air.local>
2024-11-08 15:33:12 -0500

    Ahmed's Function

--- functions.py
+++ functions.py
@@ -1,1 +1,18 @@
 # This is where we are putting our functions for the project check in
+import random
+
+def create_monster(level):
+    """
+    Creates a monster with attributes that power scales based on the given level.
+    
+    Parameters:
+    level (int): The level of the monster, which affects its health, attack, and defense power.
+    
+    Returns:
+    dict: A dictionary with the monster's attributes.
+    """
+    monster = {"Health": random.randint(75,100) + (level * 5), 
+              "Attack": random.randint(10,15) + (level * 2),
+              "Defense Power": (5 + level * 2),
+              "Name": "Monster Level " + str(level)}
+    return monster
