c67612828d80dda901c8efd85521aa07b7299182
abhisbd10 <aabhisheksubedi@gmail.com>
2024-12-02 17:56:25 -0500

    newest chnages for class player

--- functions.py
+++ functions.py
@@ -15,30 +15,8 @@ class Player:
 
     def equip_weapon(self, weapon):
          self.weapon = weapon 
-
-
-    def __add__(self):
-        #when the player picks up the health bar
-        pass
-
-def create_monster(level):
-    """
-    Creates a monster with attributes that power scales based on the given level.
-    
-    Parameters:
-    level (int): The level of the monster, which affects its health, attack, and defense power.
-    
-    Returns:
-    dict: A dictionary with the monster's attributes.
-    """
-    monster = {"Health": random.randint(75,100) + (level * 5), 
-              "Attack": random.randint(10,15) + (level * 2),
-              "Defense Power": (5 + level * 2),
-              "Name": "Monster Level " + str(level)}
-    return monster
-
-
-def playerhealth(monster_attack = False, health = 100, health_jar = False):
+        
+    def playerhealth(monster_attack = False, health = 100, health_jar = False):
 
     """ Manages the player's health based on monster attacks and found health jar.
 
@@ -58,18 +36,36 @@ def playerhealth(monster_attack = False, health = 100, health_jar = False):
 
     if monster_attack:
     # If the monster attacks the player, he will lose his health by 10.
-        health -= health_loss
-        print("Monster attacked! Player health decreased by 10.")
+        self.health -= health_loss
+        print("Monster attacked! {self.name}. Player health decreased by 10.")
     elif health_jar:
     # If the player finds the health jar, it will increase his health.
-        health += boost_health
-        print(f"Player found a health jar! Gained {boost_health} health.")
+        self.health += boost_health
+        print(f"{self.name} found a health jar! Gained {boost_health} health. Current health: {self.health}")
 
-    health = min(max(health, 0), 100)
+    self.health = min(max(health, 0), 100)
     # This will keep the player's health from going below 0 and above 100.
-    print(f"Current health: {health}")
-    return health
+    print(f"Current health: {self.health}")
+    return self.health
 
+
+
+
+def create_monster(level):
+    """
+    Creates a monster with attributes that power scales based on the given level.
+    
+    Parameters:
+    level (int): The level of the monster, which affects its health, attack, and defense power.
+    
+    Returns:
+    dict: A dictionary with the monster's attributes.
+    """
+    monster = {"Health": random.randint(75,100) + (level * 5), 
+              "Attack": random.randint(10,15) + (level * 2),
+              "Defense Power": (5 + level * 2),
+              "Name": "Monster Level " + str(level)}
+    return monster
 
 def combat_sys(player_stamina, weapon_choice, attack_choice):
     
